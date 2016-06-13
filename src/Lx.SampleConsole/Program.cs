80a72afcf3473f528ec9609b7acfbc2db580bc2d
storm <storm@leoxia.com>
2016-06-13 11:59:01 +0700

    First commit.

---
+++ src/Lx.SampleConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lx.SampleConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var myClass = new MyLibClass();
+        }
+    }
+}
---
+++ test/Lx.SampleWebApp.Test/MyWebAppTest.cs
@@ -0,0 +1,16 @@
+﻿using Lx.SampleWebApp.Services;
+using Xunit;
+
+namespace Lx.SampleWebApp.Test
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class MyWebAppTest
+    {
+        [Fact]
+        public void MainTest()
+        {
+            var myService = new MySampleService();
+        }
+    }
+}
